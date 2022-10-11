2c9e06818fb7d198b8e0ae71e901590ba1bcce42
IriS <mhuynk1005@gmail.com>
2022-10-11 12:05:35 +0700

    Change framework

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('users', { title: 'Users Page' , name: 'Huy'});
+  res.send('respond with a resource');
 });
 
 module.exports = router;
